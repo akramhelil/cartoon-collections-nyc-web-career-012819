59e0ad31f56d823bd3410b74f9df797f3fb253f6
Akram Helil <akramhelil@gmail.com>
2019-01-14 22:03:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(capitan_calls)# code an argument here
   capitan_calls.map { |call| call.capitalize + "!"  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)# code an argument here
+  planeteer_calls.any? {| call| call.length > 4 }# Your code here
 end
 
 def find_the_cheese# code an argument here
