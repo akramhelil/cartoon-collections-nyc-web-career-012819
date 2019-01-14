002d9aee63bb987fae68ec0cc38e936741eb8447
Akram Helil <akramhelil@gmail.com>
2019-01-14 22:08:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,10 @@ def long_planeteer_calls(planeteer_calls)# code an argument here
   planeteer_calls.any? {| call| call.length > 4 }# Your code here
 end
 
-def find_the_cheese(maybe_cheese)# code an argument here
+def find_the_cheese(maybe_cheese_item)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  maybe_cheese_item.find do |maybe_cheese_item|
+    cheese_types.include?(maybe_cheese_item)
+  end
 end
